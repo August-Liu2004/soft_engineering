c5f3659c619c0fe587ea9e3f46d721e4b5f24dd2
Shirley <3047994771@qq.com>
2025-03-18 10:48:55 +0800

    error delete

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def main():
     """
     Main function to print a greeting message.
     """
-    print("Hello")
+    print("Heo")
     print("hello刘昀臻")
     print("hello ntk!")
     print("add hello")
